8bb685558ef5290c780b6238c22990ce3d70b34b
Anurag Rishi <anuragrishi2907@gmail.com>
2024-05-09 08:19:15 +0530

    router and controller and registration

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-// import mongoose from "mongoose";
-// import {DB_NAME} from "./constant.js"
-// require('dotenv').config({path:'./env'})
-import dotenv from "dotenv";
-import connectDb from "./db/index.js";
-
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
-connectDb()
+
+
+connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000,() => {
-        console.log(`surver is running on port : ${process.env.PORT}`);
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
-
 })
-.catch((error) => {
-    console.log("Mongodb connection failed !!! ", error )
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
 
 
@@ -28,42 +27,25 @@ connectDb()
 
 
 
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
 
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
 
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
 
-
-
-
-
-
-
-
-
-
-
-// import express from "express";
-// const app = express()
-
-// (async () => {
-//     try{
-
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-
-//         app.on("error",(error) => {
-//             console.log("Error",error);
-//             throw error
-//         })
-
-
-//         app.listen(process.env.PORT, () =>{
-//             console.log(`app is listenign on ${process.env.PORT}`)
-//         })
-
-//     }
-//       catch (error) {
-//         console.log("error",error);
-//         throw error
-//     }
-
-    
-// })()
+*/
